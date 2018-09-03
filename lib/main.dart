6b24a490744f01800a6e1e0cdbc84886a531a511
Nick <npalme2@gamil.com>
2018-09-03 09:28:51 -0400

    Troubleshooting ListView with http request.

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:reddit_client/models/post.dart';
 
 // Entry point into the app
 void main() => runApp(RedditClient());
-//void main() => runApp(InfiniteScroll());
 
 // Main Widget
 class RedditClient extends StatelessWidget {
@@ -16,9 +15,7 @@ class RedditClient extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Today I Learned Stuffs...',
-      theme: new ThemeData(
-        primaryColor: Colors.red,
-      ),
+      theme: new ThemeData(primaryColor: Colors.red,),
       home: RedditPosts(),
     );
   }
@@ -34,39 +31,33 @@ class RedditPostsState extends State<RedditPosts> {
 
   var _posts = List<Post>();
   var _lastPostId;
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+  var _futureBuilder;
   ScrollController _scrollController = new ScrollController();
   bool isPerformingRequest = false;
+  // final _biggerFont = const TextStyle(fontSize: 18.0);
 
 
   @override
   void initState() {
+    print('initState()');
     super.initState();
-    print('In initState()');
+
+    // Create the scroll controller, which gets additional posts when at the
+    // bottom of the page.
     _scrollController.addListener(() {
-      print(_scrollController.position.pixels);
-      print(_scrollController.position.maxScrollExtent);
-      if (_posts.isNotEmpty && _scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+      if (_posts.isNotEmpty && _scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
         _lastPostId = _posts[_posts.length - 1].postId;
-        print('Got last post id: $_lastPostId');
+        print('Post count...');
+        print(_posts.length);
+        print('Got last post id: $_lastPostId, need to request more.');
         _getPosts(_lastPostId);
       }
     });
-  }
 
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-
-    var futureBuilder = new FutureBuilder(
-      future: _makeRequest(null),
+    // Included the future builder here so the async request occurs only once
+    _futureBuilder = new FutureBuilder(
+      future: _makeRequest(_lastPostId),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
         print(snapshot);
         switch (snapshot.connectionState) {
@@ -80,7 +71,7 @@ class RedditPostsState extends State<RedditPosts> {
               return new Text('Error: ${snapshot.error}');
             }
             else {
-              print("Building inital post list");
+              print("Recieved response, building inital post list");
               _posts.addAll(snapshot.data);
               return _buildPostList(context, snapshot);
             }
@@ -88,50 +79,30 @@ class RedditPostsState extends State<RedditPosts> {
       },
     );
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('/r/TodayILearned Posts'),
-      ),
-      body: futureBuilder,
-    );
   }
 
 
-  Widget _buildPostList(BuildContext context, AsyncSnapshot snapshot) {
-    List<Post> newPosts = snapshot.data;
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemCount: newPosts.length,
-        itemBuilder: (context, i) {
-          return _buildRow(newPosts[i]);
-        },
-      controller: _scrollController,
-    );
+  @override
+  void dispose() {
+    print("DISPOSE");
+    _scrollController.dispose();
+    super.dispose();
   }
 
-  Widget _buildRow(Post post) {
-    return ListTile(
-      leading: Image.network(post.imageUrl),
-      title: Text(
-        post.title,
-        style: _biggerFont,
-      )
-    );
-  }
 
   _getPosts(var lastPostId) async {
     print("isPerformingRequest:$isPerformingRequest, params:$lastPostId");
     if (!isPerformingRequest) {
-      setState(() => isPerformingRequest = true);
+      setState(() => this.isPerformingRequest = true);
 
       List<Post> newPosts = await _makeRequest(lastPostId);
       print("Got response data, setting state...");
-      print(newPosts[0].postId);
       setState(() {
-        _posts.addAll(newPosts);
-        print(_posts.length);
-        isPerformingRequest = false;
+        this._posts.addAll(newPosts);
+        this.isPerformingRequest = false;
       });
+      print(_posts.length);
+
     }
   }
 
@@ -139,7 +110,7 @@ class RedditPostsState extends State<RedditPosts> {
     List<Post> posts = new List<Post>();
     var url = "https://www.reddit.com/r/todayilearned.json";
     if(postId != null) {
-      url += '?$postId';
+      url += '?after=$postId';
     }
     print("Making request to $url");
     await http.get(url).then((res) async {
@@ -147,12 +118,48 @@ class RedditPostsState extends State<RedditPosts> {
 
       var data = decoded['data'];
       for (var post in data['children']) {
-        Post rPost = new Post(post['data']['name'], post['data']['title'],
-            post['data']['thumbnail']);
+        Post rPost = new Post(
+            post['data']['name'],
+            post['data']['title'],
+            post['data']['thumbnail']
+        );
         posts.add(rPost);
       }
     });
-    print('makeRequest():posts:');
     return posts;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('/r/TodayILearned Posts'),
+      ),
+      body: _futureBuilder,
+    );
+  }
+
+
+  Widget _buildPostList(BuildContext context, AsyncSnapshot snapshot) {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemCount: _posts.length,
+        itemBuilder: (BuildContext context, int i) {
+          print(_posts[i].postId);
+          return _buildRow(_posts[i]);
+        },
+      controller: _scrollController,
+    );
+  }
+
+  Widget _buildRow(Post post) {
+    return ListTile(
+     // leading: Image.network(post.imageUrl),
+      title: Text(
+        post.title,
+        //style: _biggerFont,
+      )
+    );
   }
 }
